f7c9c7f09386cdb58421fb665113860d379c9107
JiHoon KIM <jihoonkim@JiHoonui-MacBookAir.local>
2021-10-25 13:02:25 +0900

    [MODIFY]views_modify_v3

--- reservations/models.py
+++ reservations/models.py
@@ -1,73 +1,55 @@
-from django.db    import models
-
-from core.models  import TimeStampModel
-
-class SeatClass(models.Model):
-    name = models.CharField(max_length=50)
-
-    class Meta:
-        db_table = 'seat_classes'
-
-class FlightSchedule(models.Model):
-    depature_time = models.DateTimeField()
-    arrival_time  = models.DateTimeField()
-    airline_code  = models.CharField(max_length=50)
-
-    class Meta:
-        db_table = 'flight_schedules'
-
-class Airport(models.Model):
-    name = models.CharField(max_length=50)
-    code = models.CharField(max_length=50)
-
-    class Meta:
-        db_table = 'airports'
-
-class AirlinesSchedule(models.Model):
-    airport          = models.ForeignKey(Airport, on_delete=models.CASCADE)
-    flight_schedule  = models.ForeignKey(FlightSchedule, on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = 'airlines_schedules'
+from django.db import models
 
 class Airline(models.Model):
-    name       = models.CharField(max_length=50)
-    logo_image = models.CharField(max_length=2000)
+    name     = models.CharField(max_length=45)
+    eng_name = models.CharField(max_length=45, default='')
+    logo_url = models.URLField(max_length=1000, default='')
 
     class Meta:
         db_table = 'airlines'
 
-class Passenger(TimeStampModel):
-    first_name    = models.CharField(max_length=50)
-    last_name     = models.CharField(max_length=50)
-    nationality   = models.CharField(max_length=50)
-    date_of_birth = models.DateField()
-    gender        = models.BooleanField()
+class Airport(models.Model):
+    name     = models.CharField(max_length=45)
+    eng_name = models.CharField(max_length=45)
 
     class Meta:
-        db_table = "passengers"
+        db_table = 'airports'
 
 class Reservation(models.Model):
-    price             = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    departure_date    = models.DateField()
-    arrival_date      = models.DateField()
-    departure_airport = models.CharField(max_length=50)
-    arrival_airport   = models.CharField(max_length=50)
-    remain_seat       = models.CharField(max_length=10, null=True)
-    airline_change    = models.CharField(max_length=10)
-    seat_class        = models.ForeignKey(SeatClass, on_delete=models.SET_NULL, null=True)
-    airlines_schedule = models.ForeignKey(AirlinesSchedule, on_delete=models.SET_NULL, null=True)
-    airline           = models.ForeignKey(Airline, on_delete=models.SET_NULL, null=True)
-    passenger         = models.ForeignKey(Passenger, on_delete=models.SET_NULL, null=True)
+    airline        = models.ForeignKey(Airline, on_delete=models.CASCADE)
+    flight_code    = models.CharField(max_length=45)
+    airport_depart = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name = 'depart')
+    airport_arrive = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name = 'arrive')
+    depart_time    = models.TimeField()
+    arrive_time    = models.TimeField()
+    start_date     = models.DateField()
+    end_date       = models.DateField()
+    remain_seats   = models.IntegerField(default=0)
+    seat_class     = models.CharField(max_length=45)
+    flight_price    = models.DecimalField(max_digits = 10, decimal_places = 2, default = 0)
 
     class Meta:
         db_table = 'reservations'
 
+class Weekday(models.Model):
+    name = models.CharField(max_length=45)
+
+    class Meta:
+        db_table = 'weekdays'
+
+class FlightWeekday(models.Model):
+    flight  = models.ForeignKey(Reservation, on_delete=models.CASCADE)
+    date    = models.DateField()
+    weekday = models.CharField(max_length=45)
+
+    class Meta:
+        db_table = 'flight_weekdays'
+
 class AirplaneTaxes(models.Model):
+    reservation    = models.ForeignKey(Reservation, on_delete=models.SET_NULL, null=True)
     fuel_surcharge = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     customs_duty   = models.DecimalField(max_digits=10, decimal_places=2, default=8000)
     ticketing_fee  = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    reservation    = models.ForeignKey(Reservation, on_delete=models.SET_NULL, null=True)
 
     class Meta:
         db_table = 'airplane_taxes'
